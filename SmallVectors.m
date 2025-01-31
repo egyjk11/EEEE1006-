f8923910eb18a07ffc6729971f55c287d49b1fa9
egyjk11 <egyjk11@nottingham.ac.uk>
2025-01-31 16:37:59 +0000

    eg

---
+++ SmallVectors.m
@@ -0,0 +1,12 @@
+% Jonathon Kluth egyjk11@nottingham.ac.uk
+clear
+a = [1:10]
+b = [-pi:pi:pi]
+c = [10:-10:0]
+c = [c,c,c,c,c]
+d = [(-pi/2):(pi/4):(pi/2)]
+e = [5:2:17]
+f = [3:-1:-3]
+g = [0:0.1:1]
+   
+
