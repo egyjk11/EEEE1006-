a9fb0ac3074f78261d538e225ad8a385285b8f88
egyjk11 <egyjk11@nottingham.ac.uk>
2025-01-31 16:43:20 +0000

    aaa

---
+++ BiggerVectors.m
@@ -0,0 +1,8 @@
+    % Jonathon Kluth egyjk11@nottingham.ac.uk
+    clear
+    a = [0:20]
+    b = [0:0.5:20]
+    c = linspace(0, 200, 100)
+    d = [1:2:51]
+    e = linspace(-200, 200, 100)
+    f = [-3:1:3]
