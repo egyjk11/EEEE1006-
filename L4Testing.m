1819972eaf6aea04fd112c3e690d82b7c2bbb7e8
egyjk11 <egyjk11@nottingham.ac.uk>
2025-02-05 12:17:38 +0000

    aa

---
+++ L4Testing.m
@@ -0,0 +1,8 @@
+% Jonathon Kluth egyjk11@nottingham.ac.uk
+clear
+% Script to print Hello! 100 Times
+for n=1:100
+    disp('Hello!') %Print to screen
+    disp(sprintf('n=%d',n)); %Print current value of 
+end
+% End of program
