ec2b623d14f3966a33e9c4d711b8b168c99fbee9
egyjk11 <egyjk11@nottingham.ac.uk>
2025-02-05 12:20:36 +0000

    work

--- L4Testing.m
+++ L4Testing.m
@@ -1,8 +1,8 @@
 % Jonathon Kluth egyjk11@nottingham.ac.uk
 clear
 % Script to print Hello! 100 Times
-for n=1:100
+for n = 1:100
     disp('Hello!') %Print to screen
-    disp(sprintf('n=%d',n)); %Print current value of 
+    fprintf('n = %d\n', n) %Print current value of n
 end
 % End of program
