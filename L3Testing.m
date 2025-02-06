261a0751823024b44994a6a98f08df76079e6ce1
egyjk11 <egyjk11@nottingham.ac.uk>
2025-02-06 13:33:15 +0000

    g

---
+++ L3Testing.m
@@ -0,0 +1,14 @@
+% Jonathon Kluth egyjk11@nottingham.ac.uk
+clear
+
+message = 'The speed of an airplane is';
+speed_of_plane = 500;
+units='m/s';
+
+disp(message);
+disp(speed_of_plane);
+disp(units)
+
+
+
+
