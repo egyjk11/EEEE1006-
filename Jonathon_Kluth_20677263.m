3a5616da9403cc0c5f8c974e7aaebb1081ec9e56
egyjk11 <egyjk11@nottingham.ac.uk>
2025-02-07 16:50:22 +0000

    Started the CW document

---
+++ Jonathon_Kluth_20677263.m
@@ -0,0 +1,45 @@
+% Jonathon Kluth
+% egyjk11@nottingham.ac.uk
+
+%% Q1 - VARIABLES [8 MARKS]
+clear
+
+
+%% Q2 - VECTORS [12 MARKS]
+clear
+
+
+
+%% Q3 - SOLVING EQUATIONS [6 MARKS]
+clear
+
+
+%% Q4 - EXAMPLES [12 MARKS]
+clear
+
+
+
+%% Q5 - PROGRAM FLOW [12 MARKS]
+clear
+
+
+%% Q6 FORMAT AND PRINT TEXT TO SCREEN AND LOOPS [13 MARKS]
+clear
+
+
+
+%% Q7 - FOR LOOPS AND DISPLAYING DATA [16 MARKS]
+clear
+
+
+
+
+%% Q8 - USING THE SWITCH STATEMENT [10 MARKS]
+clear
+
+%% PRESENTATION AND FORMATTING [11 MARKS]
+
+% You do not need to put anything in this section
+% Marks will be given for neat, tidy presentation, commenting
+% and remembering to put your name and email address at the top of the
+% script
